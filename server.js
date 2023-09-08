1120bcdacc281d5eadd1054321d404364ef0e423
Himanshu <99206527+himzz1234@users.noreply.github.com>
2023-09-08 19:03:49 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,48 +1,47 @@
-const express  = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 
-app.get('/', (req, res) => {
-    res.send('api is running')
-})
+app.post("/bfhl", (req, res) => {
+  const data = req.body.data;
 
-app.post('/bfhl', (req, res) => {
-    const data = req.body.data
+  if (data && typeof data == "object") {
+    try {
+      const alphabets = data.filter((item) => item.match(/^[A-Za-z]+$/)) || [];
+      const numbers = data.filter((item) => item.match(/[0-9]/i)) || [];
 
-    if (data){
-        try {
-            const alphabets = data.filter((item) => item.match(/^[A-Za-z]+$/)) || []
-            const numbers = data.filter((item) => item.match(/[0-9]/i)) || []
-        
-            const highest = alphabets.sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0))[0] || [];
-            const resp = {
-                is_success: true,
-                user_id: "hm1624",
-                email: "hm1624@srmist.edu.in",
-                roll_number: "RA2011043020048",
-                numbers: numbers,
-                alphabets: alphabets,
-                highest_alphabet: [highest]
-            }
-        
-            res.status(200).json(resp)
-        } catch (error) {
-            res.status(404).json({error: error.message})
-        }
-    } else {
-        res.status(404).json({error: "Not a valid data"})
+      const highest =
+        alphabets.sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0))[0] || [];
+      const resp = {
+        is_success: true,
+        user_id: "hm1624",
+        email: "hm1624@srmist.edu.in",
+        roll_number: "RA2011043020048",
+        numbers: numbers,
+        alphabets: alphabets,
+        highest_alphabet: [highest],
+      };
+
+      res.status(200).json(resp);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
     }
+  } else {
+    res.status(404).json({ error: "Not a valid data" });
+  }
+});
 
-})
+app.get("/bfhl", (req, res) => {
+  res.status(200).json({ operation_code: 1 });
+});
 
-app.get('/bfhl', (req, res) => {
-  res.status(200).json({operation_code: 1})
-})
-
-app.listen('8080', () => {
-    console.log('App is running')
-})
+app.listen("8080", () => {
+  console.log("App is running");
+});
