7381a8f01298a99467aa5728dad24f2bd9ac335d
Himanshu <99206527+himzz1234@users.noreply.github.com>
2023-09-08 16:26:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,28 +5,38 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 
-app.post('/bfhl', (req, res) => {
-    const data = req.body.data
-
-    const alphabets = data.filter((item) => item.match(/^[A-Za-z]+$/)) || []
-    const numbers = data.filter((item) => item.match(/[0-9]/i)) || []
-
-    const highest = alphabets.sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0))[0] || [];
-    const resp = {
-        is_success: true,
-        user_id: "hm1624",
-        email: "hm1624@srmist.edu.in",
-        roll_number: "RA2011043020048",
-        numbers: numbers,
-        alphabets: alphabets,
-        highest_alphabet: [highest]
-    }
-
-    res.status(200).json(resp)
-})
 
 app.get('/', (req, res) => {
     res.send('api is running')
+})
+
+app.post('/bfhl', (req, res) => {
+    const data = req.body.data
+
+    if (data){
+        try {
+            const alphabets = data.filter((item) => item.match(/^[A-Za-z]+$/)) || []
+            const numbers = data.filter((item) => item.match(/[0-9]/i)) || []
+        
+            const highest = alphabets.sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0))[0] || [];
+            const resp = {
+                is_success: true,
+                user_id: "hm1624",
+                email: "hm1624@srmist.edu.in",
+                roll_number: "RA2011043020048",
+                numbers: numbers,
+                alphabets: alphabets,
+                highest_alphabet: [highest]
+            }
+        
+            res.status(200).json(resp)
+        } catch (error) {
+            res.status(404).json({error: error.message})
+        }
+    } else {
+        res.status(404).json({error: "Not a valid data"})
+    }
+
 })
 
 app.get('/bfhl', (req, res) => {
